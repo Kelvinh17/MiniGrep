46b8379cc10a3cea91d861c9d427b9c6752b3c2b
Kelvinh17 <147737844+Kelvinh17@users.noreply.github.com>
2024-02-20 20:29:32 -0400

    Add files via upload

---
+++ MiniGrep.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define WHITE   "\033[37m"
+
+int main() {
+    std::ifstream file("programacion.txt");
+    std::string word = "la";
+    std::string text;
+
+    if (file.is_open()) {
+        std::string line;
+        while (std::getline(file, line)) {
+            text += line + "\n";
+        }
+        file.close();
+
+        size_t pos = text.find(word);
+        while (pos != std::string::npos) {
+            text.replace(pos, word.length(), BLUE + word + RESET);
+            pos = text.find(word, pos + 4 + word.length());
+        }
+
+        std::cout << text << std::endl;
+    } else {
+        std::cout << "No se pudo abrir el archivo." << std::endl;
+    }
+
+    return 0;
+}
+
